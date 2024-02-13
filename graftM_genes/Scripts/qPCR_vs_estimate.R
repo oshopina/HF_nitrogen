fe75529bf2d03a1dae252c25e1e38fe91bdb53dd
Olga Shopina <uqoshopi@uq.edu.au>
2024-02-13 11:18:09 +1000

    Olga added script to plot different biomass estimations

--- graftM_genes/Scripts/qPCR_vs_estimate.R
+++ graftM_genes/Scripts/qPCR_vs_estimate.R
@@ -1,10 +1,55 @@
 library(openxlsx)
 library(ggplot2)
 library(ggpmisc)
+library(dplyr)
 
 plfa = read.xlsx('graftM_genes/Results/PLFA_ABS_counts.xlsx', rowNames = T)
+plfa_indices = read.xlsx('graftM_genes/Data/PLFA_indicators.xlsx')
+rownames(plfa_indices) = plfa_indices$X1
 qPCR_all = read.xlsx('graftM_genes/Results/qPCR_ABS_counts.xlsx', rowNames = T)
 qPCR = read.csv('graftM_genes/Data/N and 16s and ITS.csv', row.names = 1)
+env = read.csv('graftM_genes/Data/mag_env_for_shotgun_samples.csv')
+rownames(env) = env$Hoosfield.ID
+
+## Compare qPCR and qPCR estimate
+
+env_sorted = env[order(env$pH),]
+qPCR_sorted = qPCR[rownames(env_sorted),] %>% na.omit()
+plfa_ind_sorted = plfa_indices[rownames(env_sorted),] %>% na.omit()
+
+plot(qPCR_sorted$pH, qPCR_sorted$X16s.copynumber_g.dry.soil)
+
+mypal.pH <- colorRampPalette(c("#9e0142", "#d53e4f", "#f46d43", "#fdae61", "#fee08b", "#e6f598", "#abdda4", "#66c2a5", "#3288bd", "#5e4fa2"))
+
+temp = env_sorted[rownames(env_sorted) %in% rownames(qPCR_sorted),]
+ggplot(qPCR_sorted, aes(y = X16s.copynumber_g.dry.soil, x = temp$pH, color = temp$pH)) +
+  geom_point(size = 2.5) +
+  scale_color_gradientn(colours = mypal.pH(256)) +
+  theme_classic() +
+  theme(legend.position = 'none') +
+  xlab('pH') +
+  ylab('qPCR')
+
+ggplot(env_sorted, aes(y = Bact_CN_gsoil, x = as.factor(env_sorted$pH), color = pH)) +
+  geom_point(size = 2.5) +
+  scale_color_gradientn(colours = mypal.pH(256)) +
+  theme_classic() +
+  theme(legend.position = 'none',
+        axis.text.x = element_blank()) +
+  xlab('pH') +
+  ylab('qPCR estimate')
+
+temp = env_sorted[rownames(env_sorted) %in% rownames(plfa_ind_sorted),]
+ggplot(plfa_ind_sorted, aes(y = biomass, x = temp$pH, color = temp$pH)) +
+  geom_point(size = 2.5) +
+  scale_color_gradientn(colours = mypal.pH(256)) +
+  theme_classic() +
+  theme(legend.position = 'none') +
+  xlab('pH') +
+  ylab('PLFA biomass')
+
+## Compare individual genes abundances
+
 qPCR = qPCR[rownames(qPCR) %in% rownames(plfa),]
 plfa = plfa[rownames(qPCR),]
 qPCR_all = qPCR_all[rownames(qPCR),]
