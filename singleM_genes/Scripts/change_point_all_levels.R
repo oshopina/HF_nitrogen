05c9fe2786ef2c75f73c379b4ef90deb199184c3
Olga Shopina <uqoshopi@uq.edu.au>
2024-03-20 14:22:51 +1000

    Olga added script for change point analysis on all clustering levels

---
+++ singleM_genes/Scripts/change_point_all_levels.R
@@ -0,0 +1,88 @@
+library(gsveasyr)
+library(changepoint)
+library(ggridges)
+library(ggplot2)
+library(viridis)
+library(changepoint.geo)
+
+env = read.csv('graftM_genes/Data/mag_env_no_outliers.csv')
+rownames(env) = env$Hoosfield.ID
+env <- env[order(env$pH),]
+load_rarefied_gsvtables('singleM_genes/Data/', env.df = env,
+                        changeSampleName = T, refColumn = env$gsveasy_sample, 
+                        clustlvls = c(50:100))
+
+clusters = mget(ls(pattern = 'GSV'))
+rm(list = ls(pattern = 'GSV'))
+genes = unique(names(clusters$GSV_rarefied_gsvtables100))
+env$cp = c(1:117)
+pH_list = env[,c('cp', 'pH')]
+
+change_points_df = data.frame()
+for (j in names(clusters)) {
+  print(j)
+  for (i in genes) {
+    ############################### Change point analysis #########################
+    print(i)
+    df = clusters[[j]][[i]]
+    env_temp = env[rownames(env) %in% rownames(df),]
+    df = df[rownames(env_temp), ]
+    cpt = geomcp(df)
+    
+    dist_cpt = cpt.meanvar(
+      distance(cpt),
+      method = "PELT",
+      penalty = 'CROPS',
+      pen.value = c(5, 500)
+    )
+    
+    dist_var = cpts.full(dist_cpt)
+    ######################### WRITE CHOSEN NUMBER OF CHANGEPOINTS FOR ANGLE AND DISTANCE
+    if(all(is.na(dist_var))) next
+    cp_dist = sum(!is.na(dist_var[nrow(dist_var) - 1,]))
+    
+    ang_cpt = cpt.meanvar(
+      angle(cpt),
+      method = "PELT",
+      penalty = 'CROPS',
+      pen.value = c(5, 500)
+    )
+    
+    ang_var = cpts.full(ang_cpt)
+    ######################### WRITE CHOSEN NUMBER OF CHANGEPOINTS FOR ANGLE AND DISTANCE
+    cp_angle = sum(!is.na(ang_var[nrow(ang_var) - 1,]))
+    
+    change_points = c(cpts(dist_cpt, cp_dist)[!is.na(cpts(dist_cpt, cp_dist))], cpts(ang_cpt, cp_angle)[!is.na(cpts(ang_cpt, cp_angle))])
+    flattened_df = cbind(cp = change_points,
+                         Cluster = j,
+                         Gene = i)
+    flattened_df = rbind(flattened_df, flattened_df, flattened_df, flattened_df, flattened_df, flattened_df,
+                         flattened_df, flattened_df, flattened_df, flattened_df, flattened_df, flattened_df,
+                         flattened_df, flattened_df, flattened_df, flattened_df, flattened_df, flattened_df,
+                         flattened_df, flattened_df, flattened_df, flattened_df, flattened_df, flattened_df,
+                         flattened_df, flattened_df, flattened_df, flattened_df, flattened_df, flattened_df)
+    flattened_df = merge(flattened_df, pH_list, all.y = T)
+    flattened_df$Gene = i
+    flattened_df$Cluster = j
+    
+    change_points_df = rbind(change_points_df, flattened_df)
+  }
+}
+
+change_points_df$Gene[change_points_df$Gene == 'narG'] = 'narG_nxrA'
+change_points_df$Gene[change_points_df$Gene == 'narH'] = 'narH_nxrB'
+
+gene_order = openxlsx::read.xlsx('graftM_genes/Data/nitrogen_genes.xlsx')
+gene_order = gene_order[gene_order$Gene %in% unique(change_points_df$Gene),]
+change_points_df$Gene = factor(change_points_df$Gene, levels = rev(gene_order$Gene))
+change_points_df = change_points_df[,-2]
+
+mypal.pH <- colorRampPalette(c("#9e0142", "#d53e4f", "#f46d43", "#fdae61", "#fee08b", "#e6f598", "#abdda4", "#66c2a5", "#3288bd", "#5e4fa2"))
+joyplot = ggplot(change_points_df, aes(x = pH, y = Gene, fill = after_stat(x))) +
+  geom_density_ridges_gradient(scale = 2) +
+  scale_fill_gradientn(colours = mypal.pH(256)) +
+  theme_bw() +
+  theme(legend.position = 'none') +
+  xlim(3.5,8.0)
+
+
